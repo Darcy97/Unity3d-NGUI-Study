fc1fd0501608552123636cab65e6d8b25bd8655e
darcy97 <darcy97zh@gmail.com>
2017-03-23 23:36:34 -0700

    chat system

---
+++ NGUI_Test/Assets/Scripts/TestTextWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTextWindow : MonoBehaviour
+{
+
+
+    private UITextList textList;
+    private int lineNum = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        textList = this.GetComponent<UITextList>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            textList.Add("If you want it, work for it. line:" + lineNum++);
+        }
+    }
+}
