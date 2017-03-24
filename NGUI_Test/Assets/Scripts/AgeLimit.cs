410fe4d91209e7596d624ee393a11c3080356abe
darcy97 <darcy97zh@gmail.com>
2017-03-23 22:06:18 -0700

    add new scenes

---
+++ NGUI_Test/Assets/Scripts/AgeLimit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgeLimit : MonoBehaviour {
+
+    private UIInput input;
+
+    private void Awake()
+    {
+        input = this.GetComponent<UIInput>();
+
+    }
+    public void OnCommited()
+    {
+        string inputStr = input.value;
+        int intValue = int.Parse(inputStr);
+
+        if (intValue < 18)
+        {
+            input.value = "18";
+        } else if (intValue > 120)
+        {
+            input.value = "120";
+        }
+    }
+}
