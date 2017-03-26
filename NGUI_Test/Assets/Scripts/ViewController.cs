a6b18880875e4cb0056daeeea79eafd8109b8df8
darcy97 <darcy97zh@gmail.com>
2017-03-26 16:06:15 +0800

    update

---
+++ NGUI_Test/Assets/Scripts/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+
+
+    public TweenPosition optionViewAnimation;
+    public TweenPosition startViewAnimation;
+
+    public void OnFinishButtonClick()
+    {
+        print("finish");
+        optionViewAnimation.PlayReverse();
+        startViewAnimation.PlayReverse();
+    }
+
+    public void OnOptionButtonClick()
+    {
+        print("option");
+        optionViewAnimation.PlayForward();
+        startViewAnimation.PlayForward();
+    }
+}
