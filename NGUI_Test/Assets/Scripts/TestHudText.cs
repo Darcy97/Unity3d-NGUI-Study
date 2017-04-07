9e54407a567db57af6d82548f0a28f85444fd80d
darcy97 <darcy97zh@gmail.com>
2017-04-07 14:32:39 +0800

    伤害和治疗效果跟随游戏物体

---
+++ NGUI_Test/Assets/Scripts/TestHudText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHudText : MonoBehaviour {
+
+    public HUDText text;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            text.Add(-10, Color.red, 1f);
+        } else if (Input.GetMouseButtonDown(1))
+        {
+            text.Add(10, Color.green, 1f); 
+        }
+	}
+}
